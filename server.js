268cde9610bafc1715a67ba2549cfb71ff80f506
Gustavo Teodoro <gustavoalvesteodoro@gmail.com>
2017-11-26 18:08:29 -0200

    feat: WIP JWT and drafting routes

--- server.js
+++ server.js
@@ -1,46 +1,75 @@
 var express = require('express');
 var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
+var JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 mongoose.connect('mongodb://localhost:27017/authapi-experiment');
 var UserModel = require('./models/userModel');
 
-passport.use(new Strategy(
-  function(username, password, cb) {
-    UserModel.findOne({ email: username }, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user._id);
-});
-
-passport.deserializeUser(function(id, cb) {
-  UserModel.findOne({ _id: id }, function(err, user) {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
-
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-
-app.use(passport.initialize());
-app.use(passport.session());
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'authapisecretexp';
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+  UserModel.findOne({id: jwt_payload.sub}, function(err, user) {
+      if (err) {
+          return done(err, false);
+      }
+      if (user) {
+          return done(null, user);
+      } else {
+          return done(null, false);
+      }
+  });
+}));
 
 app.post('/api/user/create',
   function(req, res) {
-    res.json({
-      teste: 'home'
+    UserModel.findOne({ email: req.body.email }, function(err, user) {
+        if(user){
+          res.json({
+            mensagem: 'E-mail já existente',
+          });
+        }else {
+            var user = new UserModel({
+                nome: req.body.nome,
+                email: req.body.email,
+                senha: req.body.senha,
+                telefones: req.body.telefones,
+            });
+            user.save(function(error, user){
+                if(error) return console.error(error);
+                res.json({
+                  mensagem: 'Usuário criado com sucesso',
+                  user: user,
+                });
+            })
+        }
     });
   });
+
+app.post('/api/user/login',
+  function(req, res) {
+    UserModel.findOne({ email: req.body.email }, function(err, user) {
+        if(user){
+          if(user.senha == req.body.senha){
+            res.json(user);
+          } else {
+            res.json({
+              mensagem: "Usuário e/ou senha inválidos",
+            })
+          }
+        }else {
+          res.json({
+            mensagem: "Usuário e/ou senha inválidos",
+          })
+        }
+    });
+  });
+
 
 app.listen(3000);
