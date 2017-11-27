5ef0d29fbb59aa967d966ed3263b003fc0096d59
Gustavo Teodoro <gteodoro@hugeinc.com>
2017-11-27 13:40:41 -0200

    feat: Creating expiration rule for token

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ opts.secretOrKey = 'authapisecretexp';
 passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
   UserModel.findOne({id: jwt_payload.sub}, function(err, user) {
       if (err) {
-          return done(err, false);
+          return (err, false);
       }
       if (user) {
           return done(null, user);
@@ -60,7 +60,8 @@ app.post('/api/user/login',
       if(err) return res.status(500).json({erro: err});
       if(user){
         if(user.senha == req.body.senha){
-          const token = jwt.encode({ _id: user._id }, opts.secretOrKey);
+          const expDate = Math.round(Date.now() / 1000 + 30 * 60);
+          const token = jwt.encode({ _id: user._id, exp: expDate }, opts.secretOrKey);
           res.json(Object.assign({}, {user}, {token}));
         } else {
           res.status(401).json({
@@ -74,8 +75,31 @@ app.post('/api/user/login',
       }
     });
   });
+  
+function authentication(req, res, next) {
+    passport.authenticate('jwt', { session: false }, (err, user, info) => {
+        if (info && info.name === 'JsonWebTokenError') {
+            return res.status(403).json({
+                erro: 'Token Inválido',
+            });
+        }
 
-app.get('/api/user', passport.authenticate('jwt', { session: false }),
+        if (info && info.name === 'TokenExpiredError') {
+            return res.status(403).json({
+                erro: 'Token expirado',
+            });
+        }
+
+        if (err) {
+            return next(err);
+        }
+
+        req.user = user;
+        return next();
+    })(req, res, next);
+}  
+
+app.get('/api/user', authentication,
   function(req, res) {
     res.json(req.user);
   });
